e244307c2e719369d3157268861b5409bc5ceb74
RinHizakura <s921975628@gmail.com>
2020-10-23 14:44:30 +0800

    Fix rendering error of floating-point version
    In floating-point implementation, function 'Trace' in
    raycaster_float.cpp will assign 'INV_FACTOR / distance'
    to an uint8_t variable 'screenY', which may come into an
    overflow problem, leading to unexpected render.
    
    To fix the problem, we can just assign 'screenY' to
    'SCREEN_HEIGHT>>1' for overflow case, since this will
    meet what the caller of 'Trace' expect for.

--- raycaster_float.cpp
+++ raycaster_float.cpp
@@ -148,11 +148,13 @@ void RayCasterFloat::Trace(uint16_t screenX,
     *textureY = 0;
     *textureStep = 0;
     if (distance > 0) {
-        *screenY = INV_FACTOR / distance;
-        auto txs = (*screenY * 2.0f);
+        uint16_t tmpY = INV_FACTOR / distance;
+        auto txs = (tmpY * 2.0f);
+        *screenY = tmpY;
         if (txs != 0) {
             *textureStep = (256 / txs) * 256;
             if (txs > SCREEN_HEIGHT) {
+                *screenY = SCREEN_HEIGHT >> 1;
                 auto wallHeight = (txs - SCREEN_HEIGHT) / 2;
                 *textureY = wallHeight * (256 / txs) * 256;
             }
